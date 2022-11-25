9970fbaf200169d5cc154b66e757a4885a330ced
Pietro <pietro.aquino@tce.ma.gov.br>
2022-11-25 10:22:05 -0300

    primeiro commit

---
+++ user-service/src/main/java/com/example/user/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.user.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "usuario")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Long departmentId;
+
+}
